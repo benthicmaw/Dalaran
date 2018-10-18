b9ef482c83e2c57ae034eb020f9f933a5994094d
Kai Chang <kaijchang@gmail.com>
2018-10-17 19:11:43 -0700

    action parsing

--- Dalaran/parser/parser.py
+++ Dalaran/parser/parser.py
@@ -1,28 +1,59 @@
 from fireplace.cards.utils import *
 
+from .utils import *
+
 
 def parse_tokens(tokens):
     tags = {}
+    attributes = {}
 
-    pos = 0
+    tokens = iter(tokens)
 
-    while pos <= len(tokens) - 1:
-        token = tokens[pos]
+    in_ability_block = True
+
+    while True:
+        try:
+            token = next(tokens)
+
+        except StopIteration:
+            break
 
         token_type = token[1]
         token_value = token[0]
 
-        if token_type == "ABILITY":
-            if all(token[1] == "ABILITY" for token in tokens[:pos]):
+        if token_type == 'ABILITY':
+            if in_ability_block:
                 tags[GameTag[token_value.upper()]] = True
 
-        pos += 1
+        elif token_type == 'ACTION':
+            in_ability_block = False
 
-    return tags
+            action = globals().get(token_value.lower().capitalize(), None)
+            assert isinstance(action, ActionMeta)
+
+            target = CONTROLLER
+
+            next_token = next(tokens)
+
+            if next_token[1] == 'FIELD':
+                amount = 1
+
+            elif next_token[1] == 'INT':
+                if next_token[0].isdigit():
+                    amount = int(next_token[0])
+
+                else:
+                    amount = word_to_num(next_token[0])
+
+                next(tokens)
+
+            attributes['play'] = action(target) * amount
+
+    return tags, attributes
 
 
 def parse_card(name, type_, cost, class_, tokens, atk=None, health=None):
-    tags = parse_tokens(tokens)
+    tags, attributes = parse_tokens(tokens)
 
     tags[GameTag.CARDNAME] = name
     tags[GameTag.COST] = cost
@@ -37,7 +68,10 @@ def parse_card(name, type_, cost, class_, tokens, atk=None, health=None):
     if health is not None:
         tags[GameTag.HEALTH] = health
 
-    card = type('CUSTOM_{}'.format(name), (), {"tags": tags})
+    values = {'tags': tags}
+    values.update(attributes)
+
+    card = type('CUSTOM_{}'.format(name), (), values)
     return card
 
 
--- tests/test.py
+++ tests/test.py
@@ -1,8 +1,5 @@
 import unittest
 
-import json
-import os
-
 from .utils import *
 
 from Dalaran.lexer import lex, token_exprs
@@ -11,33 +8,26 @@ from Dalaran.parser import parse_card, register_card
 
 
 class Test_Cards(unittest.TestCase):
-    def setUp(self):
-        with open(os.path.join(os.path.dirname(__file__), 'RecklessRocketeer.json')) as card:
-            self.card_data = json.load(card)
-
     def test_tokenization(self):
-        tokens = lex(self.card_data['text'], token_exprs)
+        tokens = lex('draw two cards.', token_exprs)
 
-        self.assertEqual(len(tokens), 1)
+        self.assertListEqual(
+            tokens, [('draw', 'ACTION'), ('two', 'INT'), ('cards', 'FIELD')])
 
     def test_parsing(self):
-        tokens = lex(self.card_data['text'], token_exprs)
+        tokens = lex('Charge. Windfury.', token_exprs)
 
-        card = parse_card(self.card_data['name'], self.card_data['type'], self.card_data['cost'], self.card_data['class'],
-                          tokens, self.card_data.get('attack', None), self.card_data.get('health', None))
+        card = parse_card('Charge Windfury Boi', 'Minion', 4, 'Neutral',
+                          tokens, 3, 2)
 
-        self.assertEqual(card.tags[GameTag.CHARGE], True)
-        self.assertEqual(card.tags[GameTag.CARDNAME], 'Reckless Rocketeer')
-        self.assertEqual(card.tags[GameTag.CARDTYPE], CardType.MINION)
-        self.assertEqual(card.tags[GameTag.CLASS], CardClass.NEUTRAL)
-        self.assertEqual(card.tags[GameTag.ATK], 5)
-        self.assertEqual(card.tags[GameTag.HEALTH], 2)
+        self.assertDictEqual({GameTag.CHARGE: True, GameTag.WINDFURY: True, GameTag.CARDNAME: 'Charge Windfury Boi', GameTag.COST: 4,
+                              GameTag.CARDTYPE: CardType.MINION, GameTag.CLASS: CardClass.NEUTRAL, GameTag.ATK: 3, GameTag.HEALTH: 2}, card.tags)
 
-    def test_playing(self):
-        tokens = lex(self.card_data['text'], token_exprs)
+    def test_card_abilities(self):
+        tokens = lex('Charge. Windfury.', token_exprs)
 
-        card = parse_card(self.card_data['name'], self.card_data['type'], self.card_data['cost'], self.card_data['class'],
-                          tokens, self.card_data.get('attack', None), self.card_data.get('health', None))
+        card = parse_card('Charge Windfury Boi', 'Minion', 4, 'Neutral',
+                          tokens, 3, 2)
 
         register_card(card)
 
@@ -50,4 +40,48 @@ class Test_Cards(unittest.TestCase):
 
         rocketeer.attack(game.current_player.opponent.hero)
 
+        self.assertTrue(rocketeer.can_attack())
+
+        rocketeer.attack(game.current_player.opponent.hero)
+
         self.assertFalse(rocketeer.can_attack())
+
+    def test_card_actions(self):
+        tokens = lex('Draw a Card', token_exprs)
+        card_1 = parse_card('a', 'Spell', 2, 'Mage', tokens)
+
+        tokens = lex('Draw two Cards', token_exprs)
+        card_2 = parse_card('two', 'Spell', 3, 'Mage', tokens)
+
+        tokens = lex('Draw 2 Cards', token_exprs)
+        card_3 = parse_card('2', 'Spell', 3, 'Mage', tokens)
+
+        register_card(card_1)
+        register_card(card_2)
+        register_card(card_3)
+
+        game = prepare_game(CardClass.MAGE, CardClass.MAGE)
+
+        self.assertEqual(len(game.current_player.hand), 4)
+
+        a = game.player1.give(card_1.__name__)
+        two = game.player1.give(card_2.__name__)
+        number_two = game.player1.give(card_3.__name__)
+
+        self.assertEqual(len(game.current_player.hand), 7)
+        self.assertEqual(game.current_player.cards_drawn_this_turn, 1)
+
+        a.play()
+
+        self.assertEqual(len(game.current_player.hand), 7)
+        self.assertEqual(game.current_player.cards_drawn_this_turn, 2)
+
+        two.play()
+
+        self.assertEqual(len(game.current_player.hand), 8)
+        self.assertEqual(game.current_player.cards_drawn_this_turn, 4)
+
+        number_two.play()
+
+        self.assertEqual(len(game.current_player.hand), 9)
+        self.assertEqual(game.current_player.cards_drawn_this_turn, 6)
