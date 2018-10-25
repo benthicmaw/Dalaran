1ad9a2d7957a75808880fe8c0ef1f9c3501bdf29
Kai Chang <kaijchang@gmail.com>
2018-10-24 20:22:53 -0700

    actually get lexer working

--- Dalaran/lexer.py
+++ Dalaran/lexer.py
@@ -1,9 +1,9 @@
-import re
-
 from pyleri import *
 
 
-class Numbers:
+class Hearthstone_Grammar(Grammar):
+    # NUMBERS
+
     number_words = Choice(Keyword('one', ign_case=True), Keyword('two', ign_case=True), Keyword('three', ign_case=True), Keyword('four', ign_case=True), Keyword(
         'five', ign_case=True), Keyword('six', ign_case=True), Keyword('seven', ign_case=True), Keyword('eight', ign_case=True), Keyword('nine', ign_case=True), Keyword('ten', ign_case=True))
 
@@ -14,8 +14,8 @@ class Numbers:
 
     number = Choice(numbers_digits, number_words, special_number_words)
 
+    # ACTIONS
 
-class Actions:
     destroy = Keyword('destroy', ign_case=True)
     deal = Keyword('deal', ign_case=True)
     draw = Keyword('draw', ign_case=True)
@@ -23,11 +23,11 @@ class Actions:
     gain = Keyword('gain', ign_case=True)
     discard = Keyword('discard', ign_case=True)
 
-    action = Choice(destroy, deal, draw, freeze, gain, discard)
+    # action = Choice(destroy, deal, draw, freeze, gain, discard)
 
+    # ABILITIES
 
-class Abilities:
-    spell_damage = Keyword('spell damage', ign_case=True)
+    # spell_damage = Keyword('spell damage', ign_case=True)
 
     charge = Keyword('charge', ign_case=True)
     taunt = Keyword('taunt', ign_case=True)
@@ -36,60 +36,97 @@ class Abilities:
 
     ability = Choice(charge, taunt, divine_shield, windfury)
 
+    # EVENTS
 
-class Events:
+    """
     battlecry = Keyword('battlecry:', ign_case=True)
     deathrattle = Keyword('deathrattle:', ign_case=True)
 
     event = Choice(battlecry, deathrattle)
+    """
 
+    # TARGETS
 
-class Targets:
-    character = Choice(Keyword('character', ign_case=True),
-                       Keyword('characters', ign_case=True))
+    # character = Choice(Keyword('character', ign_case=True),
+    #                   Keyword('characters', ign_case=True))
 
     minion = Choice(Keyword('minion', ign_case=True),
                     Keyword('minions', ign_case=True))
 
+    """
     opponent = Keyword('opponent', ign_case=True)
     owner = Keyword('owner', ign_case=True)
 
     target = Choice(character, minion, opponent, owner)
+    """
 
+    # MODIFIERS
 
-class Modifiers:
+    """
     friendly = Keyword('friendly', ign_case=True)
     enemy = Keyword('enemy', ign_case=True)
     all_ = Keyword('all', ign_case=True)
 
     modifier = Choice(friendly, enemy, all_)
+    """
 
+    # FIELDS
 
-class Fields:
     card = Choice(Keyword('card', ign_case=True),
                   Keyword('cards', ign_case=True))
     damage = Keyword('damage', ign_case=True)
     armor = Keyword('armor', ign_case=True)
 
-    field = Choice(card, damage, armor)
+    # field = Choice(card, damage, armor)
 
+    # SEQUENCES
 
-class Sequences(Numbers, Actions, Abilities, Events, Targets,
-                Modifiers, Fields):
-    draw_sequence = Sequence(Actions.draw, Numbers.number, Fields.card)
-    discard_sequence = Sequence(Actions.discard, Numbers.number, Fields.card)
-    deal_sequence = Sequence(Actions.deal, Numbers.number, Fields.damage)
-    destroy_sequence = Sequence(Actions.destroy, Targets.minion)
-    freeze_sequence = Sequence(Actions.freeze, Targets.minion)
+    draw_sequence = Sequence(draw, number, card)
+    discard_sequence = Sequence(discard, number, card)
+    deal_sequence = Sequence(deal, number, damage)
+    armor_sequence = Sequence(gain, number, armor)
+    destroy_sequence = Sequence(destroy, minion)
+    freeze_sequence = Sequence(freeze, minion)
 
     action_sequence = Choice(draw_sequence, discard_sequence,
-                             deal_sequence, destroy_sequence, freeze_sequence)
+                             deal_sequence, destroy_sequence, freeze_sequence, armor_sequence)
 
-    ability_sequence = Sequence(Abilities.ability)
+    ability_sequence = Sequence(ability)
 
+    # MAIN
 
-class Main(Grammar, Sequences):
     # ID = Regex("[A-Za-z][A-Za-z0-9\']*")
 
-    START = Repeat(Choice(Sequences.action_sequence,
-                          Sequences.ability_sequence), 0)
+    START = Repeat(Choice(action_sequence, ability_sequence), 0)
+
+
+# Returns properties of a node object as a dictionary:
+def node_props(node, children):
+    return {
+        'start': node.start,
+        'end': node.end,
+        'name': node.element.name if hasattr(node.element, 'name') else None,
+        'element': node.element.__class__.__name__,
+        'string': node.string,
+        'children': children}
+
+
+# Recursive method to get the children of a node object:
+def get_children(children):
+    return [node_props(c, get_children(c.children)) for c in children]
+
+
+# View the parse tree:
+def view_parse_tree(res):
+    start = res.tree.children[0] \
+        if res.tree.children else res.tree
+    return node_props(start, get_children(start.children))
+
+
+if __name__ == '__main__':
+    import json
+
+    hearthstone_grammar = Hearthstone_Grammar()
+    res = hearthstone_grammar.parse('Deal 5 Damage')
+
+    print(json.dumps(view_parse_tree(res), indent=2))
